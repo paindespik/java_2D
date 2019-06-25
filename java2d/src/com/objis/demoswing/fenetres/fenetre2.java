527966c72c1fe2f1e395ae9f37dbfe1539211083
paindespik <seandriard3@gmail.com>
2019-06-26 00:36:05 +0200

    new commit

--- java2d/src/com/objis/demoswing/fenetres/fenetre2.java
+++ java2d/src/com/objis/demoswing/fenetres/fenetre2.java
@@ -1,19 +1,16 @@
-package java2d;
+package com.objis.demoswing.fenetres;
 
 import javax.swing.JFrame;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Instancier une JFrame
+public class fenetre2 extends JFrame{
+	
+	fenetre2(){
 		JFrame maFenetre1 = new JFrame();	
 		maFenetre1.setSize(400, 300);
 		maFenetre1.setTitle("votre première fenêtre SWING avec Objis");
 		maFenetre1.setLocationRelativeTo(null);
 		maFenetre1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		maFenetre1.setResizable(false);
-		// TODO Afficher la JFrame
-		maFenetre1.setVisible(true);
 	}
 
 }
